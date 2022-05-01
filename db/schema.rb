041c5132cd08c8b8b46cfa4480bd3f6f1b4bf0e9
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-05-01 00:00:35 +0000

    [UPDATE]cancancan

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_27_093354) do
+ActiveRecord::Schema.define(version: 2022_04_30_224914) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 2022_04_27_093354) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
