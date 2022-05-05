0e144552dd37462895bba8a0445e495a13bf6b59
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-05-05 07:56:46 +0000

    [UPDATE]コメントの訂正

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,11 +5,6 @@
 
 #   def initialize(user)
 #     end_user ||= EndUser.new
-    
-#     # roleがadminのユーザーはモデルの操作を行うことができて管理者画面を閲覧可能
-#     if current_end_user.role(:admin)
-#       can :manage, :all
-#     end
   
 #   end
 # end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,3 @@
 class Role < ApplicationRecord
-  # has_many :admins, dependent: :destroy
-  # has_many :end_users, dependent: :destroy
-  
-  # validates :name, presence: true
+ 
 end
