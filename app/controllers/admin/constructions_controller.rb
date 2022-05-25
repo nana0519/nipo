84ccedf9c5344f81af8568f9c8e3c7a639885770
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-05-25 08:20:01 +0000

    [UPDATE]現場詳細画面・編集画面

--- app/controllers/admin/constructions_controller.rb
+++ app/controllers/admin/constructions_controller.rb
@@ -18,6 +18,23 @@ class Admin::ConstructionsController < ApplicationController
     @constructions = Construction.page(params[:page])
   end
   
+  def show
+    @construction = Construction.find(params[:id])
+  end
+  
+  def edit
+    @construction = Construction.find(params[:id])
+  end
+  
+  def update
+    @construction = Construction.find(params[:id])
+    if @construction.update(construction_params)
+      redirect_to admin_construction_path(@construction)
+    else
+      render "edit"
+    end
+  end
+  
   private
 
   def construction_params
