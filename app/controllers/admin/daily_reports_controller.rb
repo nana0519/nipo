051513df193751ca838682833782255322660bfa
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-05-30 13:41:47 +0000

    [UPDATE]日報create

--- app/controllers/admin/daily_reports_controller.rb
+++ app/controllers/admin/daily_reports_controller.rb
@@ -7,6 +7,14 @@ class Admin::DailyReportsController < ApplicationController
   
   def create
     @daily_report = DailyReport.new(daily_report_params)
+    new_end_user = params[:daily_report][:end_user_id]
+    new_company = params[:daily_report][:company_id]
+    new_construction = params[:daily_report][:construction_id]
+    @daily_report.admin_id = current_admin.id
+    if @daily_report.update
+      @daily_report.save_daily_report(new_end_user, new_company, new_construction)
+      redirect_to admin_daily_reports_path
+    end
   end
   
   private
--- app/models/daily_report.rb
+++ app/models/daily_report.rb
@@ -4,4 +4,10 @@ class DailyReport < ApplicationRecord
   has_many :constructions, dependent: :destroy
     
   has_many_attached :daily_report_images
+  
+  def save_daily_report(new_end_user, new_company, new_construction)
+    self.end_users << new_end_user
+    self.companies << new_company
+    self.constructions << new_construction
+  end
 end
