dbd7005bff761100f6c4749246b95cd440120d11
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-06-08 05:54:06 +0000

    [UPDATE]日報詳細画面

--- app/controllers/admin/daily_reports_controller.rb
+++ app/controllers/admin/daily_reports_controller.rb
@@ -8,7 +8,7 @@ class Admin::DailyReportsController < ApplicationController
   def create
     @daily_report = DailyReport.new(daily_report_params)
     if @daily_report.save
-      redirect_to admin_daily_report_path(@daily_report)
+      redirect_to constructions_index_admin_constructions_path
     else
       render "new"
     end
@@ -21,6 +21,6 @@ class Admin::DailyReportsController < ApplicationController
   private
 
    def daily_report_params
-    params.require(:daily_report).permit(:date, :end_user_id, :company_id, :construction_id, daily_report_images: [])
+    params.require(:daily_report).permit(:date, :end_user_id, :company_id, :construction_id, :car_id, daily_report_images: [])
    end
 end
